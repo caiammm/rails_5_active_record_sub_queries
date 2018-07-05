da73ae3e085afc43fe3d5ef59b153febb138a78f
Renato Caiam <caiam.rct@gmail.com>
2018-07-05 13:37:03 -0300

    fix queries to show the examples

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,13 +2,13 @@ class Comment < ApplicationRecord
   belongs_to :article
 
   scope :latest_by_article, -> {
-    where(id: Comment.select("DISTINCT ON (article_id) article_id")
-    .order("article_id, article.created_at DESC"))
+    where(id: Comment.select("DISTINCT ON (article_id) id").
+    order("article_id, comments.created_at DESC"))
   }
 
   scope :latest_by_article_with_unscoped, -> {
-    where(id: Comment.unscoped.select("DISTINCT ON (article_id) article_id")
-    .order("article_id, article.created_at DESC"))
+    where(id: Comment.unscoped.select("DISTINCT ON (article_id) id").
+    order("article_id, comments.created_at DESC"))
   }
 
   # correct SQL from both scopes should be:
@@ -16,29 +16,29 @@ class Comment < ApplicationRecord
   # FROM "comments"
   # WHERE "comments"."id"
   # IN (
-  #   SELECT DISTINCT ON (article_id) article_id
+  #   SELECT DISTINCT ON (article_id) id
   #   FROM "comments"
-  #   ORDER BY article_id, article.created_at DESC
+  #   ORDER BY article_id, comments.created_at DESC
   # )
 end
 
 # puts Comment.where(article_id: [1,2]).latest_by_article.to_sql
 # SELECT "comments".*
 # FROM "comments"
-# WHERE "comments"."article_id" IN (1, 2) AND "comments"."id"
-# IN (
-#=> SELECT DISTINCT ON (article_id) article_id
-#=> FROM "comments"
-#=> WHERE "comments"."article_id" IN (1, 2)
-#=> ORDER BY article_id, article.created_at DESC
+# WHERE "comments"."article_id" IN (1, 2)
+# AND "comments"."id" IN (
+#   SELECT DISTINCT ON (article_id) id
+#   FROM "comments"
+#   WHERE "comments"."article_id" IN (1, 2)
+#   ORDER BY article_id, comments.created_at DESC
 # )
 
 # puts Comment.where(article_id: [1,2]).latest_by_article_with_unscoped.to_sql
 # SELECT "comments".*
 # FROM "comments"
-# WHERE "comments"."article_id" IN (1, 2) AND "comments"."id"
-# IN (
-#=> SELECT DISTINCT ON (article_id) article_id
-#=> FROM "comments"
-#=> ORDER BY article_id, article.created_at DESC
+# WHERE "comments"."article_id" IN (1, 2)
+# AND "comments"."id" IN (
+#   SELECT DISTINCT ON (article_id) id
+#   FROM "comments"
+#   ORDER BY article_id, comments.created_at DESC
 # )
